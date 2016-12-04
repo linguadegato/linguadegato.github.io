fb4799c42f120f74ce67b8394305f97bdeb30610
andreitognolo <andreitognolo@gmail.com>
2016-12-04 22:34:02 +0100

    Improve deploy script

--- cmds/deploy.sh
+++ cmds/deploy.sh
@@ -1,7 +1,12 @@
-git checkout master
-cp -r build/* .
-git add .
-git ci -m "Deploy"
-git push
+if git diff-index --quiet HEAD --; then
+  git checkout master
+  cp -r build/* .
+  git add .
+  git ci -m "Deploy"
+  git push
 
-git checkout source
+  git checkout source
+else
+  echo "You have local changes, please commit it before"
+  exit 1
+fi
